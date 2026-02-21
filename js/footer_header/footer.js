efac4c12ce0512a77c62f2605cadcaced45b015a
RMB <brioudesromain59@gmail.com>
2026-02-21 20:05:11 +0100

    Page login et trjs + 2 css

--- js/footer_header/footer.js
+++ js/footer_header/footer.js
@@ -1,8 +1,8 @@
 function footer_page(){
     const div_fo = document.getElementById("footer_dv");
     if(!div_fo)return;
-    let ft= '<p> A propos</p>';
-    ft += '<p>Nous contacter</p>';
+    let ft= '<h3 class="titre"> A propos</h3>';
+    ft += '<h3 class="titre">Nous contacter</h3>';
     div_fo.innerHTML = ft;
 }
 
--- js/footer_header/header.js
+++ js/footer_header/header.js
@@ -1,8 +1,11 @@
 function header_page(){
     const div_he = document.getElementById("header_dv");
     if(!div_he) return;
-    let ht = '<a href="/index.html">Accueil</a>';
-    ht += '<a href="/html/page1.html">Page 1</a>';
+    let ht = '<a href="index.html"><h1 class="titre">Accueil</h1></a>';
+    ht += '<a href="page1.html"><h1 class="titre">Page 1</h1></a>';
+    ht += '<a href="page2.html"><h1 class="titre">Page 2</h1></a>';
+    ht += '<a href="login.html"><h1 class="titre">Se connecter</h1></a>';
+
     div_he.innerHTML = ht;
 }
 
