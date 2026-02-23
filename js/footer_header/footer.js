eaacc378cfd10e123d5d38e3b3ec3684f039ce22
RMB <brioudesromain59@gmail.com>
2026-02-23 09:47:41 +0100

    aspect général

--- js/footer_header/footer.js
+++ js/footer_header/footer.js
@@ -1,8 +1,8 @@
 function footer_page(){
     const div_fo = document.getElementById("footer_dv");
     if(!div_fo)return;
-    let ft= '<h3 class="titre"> A propos</h3>';
-    ft += '<h3 class="titre">Nous contacter</h3>';
+    let ft= '<h3 class="titre_header"> A propos</h3>';
+    ft += '<h3 class="titre_header">Nous contacter</h3>';
     div_fo.innerHTML = ft;
 }
 
