b31818587486b8971b1f840338688348b2d90097
Alem <mael.albrecht1@gmail.com>
2026-02-24 12:12:35 +0100

    erreur liaison

--- js/footer_header/header.js
+++ js/footer_header/header.js
@@ -1,8 +1,8 @@
 function header_page(){
     const div_he = document.getElementById("header_dv");
     if(!div_he) return;
-    let ht = '<a href="/index.html">Accueil</a>';
-    ht += '<a href="/html/page1.html">Page 1</a>';
+    let ht = '<a href="/mael/index.html">Accueil</a>';
+    ht += '<a href="/mael/html/page1.html">Page 1</a>';
     div_he.innerHTML = ht;
 }
 
